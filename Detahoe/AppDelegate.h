2dcc86a124872ffea70030057b99260c94ce74be
Christopher Bruno <cbruno@cocoawerks.com>
2026-08-30 06:10:38 -0500

    Initial Commit

---
+++ Detahoe/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Detahoe
+//
+//  Created by Christopher Bruno on 8/30/26.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Detahoe/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Detahoe
+//
+//  Created by Christopher Bruno on 8/30/26.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
